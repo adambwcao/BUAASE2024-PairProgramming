dcb6438eaa7f020c2f1fd61e56359023b3ca0905
adambwcao <adambwcao@sina.com>
2024-04-04 17:39:32 +0800

    TaskThreeEasyIsPower

--- T3/t3-2-as/assembly/index.ts
+++ T3/t3-2-as/assembly/index.ts
@@ -2,70 +2,17 @@ export function mancalaOperator(flag: i32, status: i32[]): i32 {
   let maxNum: i32 = 0, maxStep: i32 = 0;
   let chess: i32[] = new Array<i32>(15).fill(4);
   if(flag == 1){
-    for (let i: i32 = 0; i <= 5; ++i) {
+    for (let i: i32 = 5; i >= 0; --i) {
       if(status[i] != 0) {
-        for (let j: i32 = 1; j <= 14; ++j) {
-          chess[j] = status[j-1];
-        }
-        let num: i32 = 0;
-        chess[i+1]=0;
-        for(let j: i32 = 0, posPut: i32 = i+2 ; j<status[i] ; ++j, posPut = posPut%13+1){ 
-          if(j == status[i]-1) {
-            if(1<=posPut && posPut<=6){
-              if(chess[posPut] == 0 && chess[14-posPut]>0){
-                chess[posPut]--;
-                chess[7] += 1 + chess[14-posPut];
-                chess[14-posPut] = 0; 
-              }
-            }
-          }
-          chess[posPut]++; 
-        }
-
-        for (let j: i32 = 1; j <= 7; ++j) {
-          num += chess[j];
-        }
-        if (num > maxNum) {
-          maxNum = num;
-          maxStep = i;
-        }
+        return  i+11;
       }
     }
-    return maxStep+11;
   } else{
-    for (let i: i32 = 7; i <= 12; ++i) {
+    for (let i: i32 = 12; i >= 7; --i) {
       if(status[i] != 0) {
-        for (let j: i32 = 1; j <= 14; ++j) {
-          chess[j] = status[j-1];
-        }
-        let num: i32 = 0;
-        chess[i+1]=0;
-        for(let j: i32 = 0, posPut: i32 = i+2 ; j<status[i] ; ++j, posPut = posPut%14+1){ 
-          if(posPut==7) {
-            j--;
-            continue;
-          }
-          if(j == status[i]-1) {
-            if(8<=posPut && posPut<=13){
-              if(chess[posPut] == 0 && chess[14-posPut]>0){
-                chess[posPut]--;
-                chess[14] += 1 + chess[14-posPut];
-                chess[14-posPut] = 0;
-              }
-            }
-          }
-          chess[posPut]++; 
-        }
-        for (let j: i32 = 8; j <= 14; ++j) {
-          num += chess[j];
-        }
-        if (num > maxNum) {
-          maxNum = num;
-          maxStep = i;
-        }
+        return i+14;
       }
     }
-    return maxStep+14;
   }
   return 0; 
 }

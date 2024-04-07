9599ede6f3a5c06f34dbfa9694464b1b314048fb
adambwcao <adambwcao@sina.com>
2024-04-07 13:24:23 +0800

    FinalPush

--- T3/t3-2-as/assembly/index.ts
+++ T3/t3-2-as/assembly/index.ts
@@ -1,6 +1,6 @@
 export function mancalaOperator(flag: i32, status: i32[]): i32 {
   let maxNum: i32 = 0, maxStep: i32 = 0;
-  let chess: i32[] = new Array<i32>(15).fill(4);
+  let chess: i32[] = new Array<i32>(15).fill(4); // init
   if(flag == 1){
     for (let i: i32 = 5; i >= 0; --i) {
       if(status[i] != 0) {
